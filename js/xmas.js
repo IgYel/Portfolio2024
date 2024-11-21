b199578b6f43ed269efeb511ca5dffa818284233
IgYel <cutelyfoxy@gmail.com>
2024-11-21 15:31:47 +0100

    adding merry Christmas

---
+++ js/xmas.js
@@ -0,0 +1,14 @@
+let currentMonth = "no";
+const xmasHat = document.querySelector('#xmasHat');
+
+const getCurrentMonth = () => {
+    const date = new Date();
+    const month = date.getMonth() + 1; // getMonth возвращает месяцы от 0 до 11, поэтому добавляем 1
+    currentMonth = month;
+};
+getCurrentMonth();
+
+if(currentMonth == 11 || currentMonth == 1){
+    snow.style.display = "block";
+    xmasHat.style.display = "block";
+}
